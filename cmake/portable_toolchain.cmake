169cc49ac3c305d91b9f70fc7a7ecd09c1cee2ed
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2019-08-30 17:30:37 +0200

    Always include the portable toolchain cmake
    Unfortunately system name detection is done when project() is called
    which is also when compiler detection is done, and we need the compiler
    to be set before that.
    So we always include the cmake file.

--- cmake/portable_toolchain.cmake
+++ cmake/portable_toolchain.cmake
@@ -1,4 +1,4 @@
-set(OSQUERY_PORTABLE_TOOLCHAIN_SYSROOT "" CACHE PATH "Path that is the sysroot that contains the portable toolchain to use to compile osquery")
+set(OSQUERY_PORTABLE_TOOLCHAIN_SYSROOT "" CACHE PATH "Path to the sysroot that contains the portable toolchain to use to compile osquery. Linux only.")
 
 if(OSQUERY_PORTABLE_TOOLCHAIN_SYSROOT)
   overwrite_cache_variable("CMAKE_C_COMPILER" "STRING" "${OSQUERY_PORTABLE_TOOLCHAIN_SYSROOT}/usr/bin/clang")
