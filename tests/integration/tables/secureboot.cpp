11d9cd0aa5c9164c55882f372d04f16424878ac7
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2022-08-23 22:12:55 +0200

    temp: Move SEH handling code

--- tests/integration/tables/secureboot.cpp
+++ tests/integration/tables/secureboot.cpp
@@ -7,9 +7,13 @@
  * SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
  */
 
+#include <osquery/logger/logger.h>
 #include <osquery/tests/integration/tables/helper.h>
-
 #include <osquery/utils/info/platform_type.h>
+
+#include <Windows.h>
+
+#include <DbgHelp.h>
 
 namespace osquery::table_tests {
 
@@ -20,8 +24,107 @@ class Secureboot : public testing::Test {
   }
 };
 
+static std::string getStack(CONTEXT& context) {
+  BOOL result;
+  HANDLE process;
+  HANDLE thread;
+  STACKFRAME64 stack;
+  constexpr std::uint32_t symbol_name_size = 1024;
+
+  char symbol_mem[sizeof(IMAGEHLP_SYMBOL64) + symbol_name_size];
+  IMAGEHLP_SYMBOL64* symbol = (IMAGEHLP_SYMBOL64*)symbol_mem;
+  DWORD64 displacement;
+
+  std::string name(symbol_name_size, '\0');
+  std::string out;
+  out += "Crash callstack:\n";
+  memset(&stack, 0, sizeof(STACKFRAME64));
+
+  process = GetCurrentProcess();
+  thread = GetCurrentThread();
+  displacement = 0;
+  DWORD machineType;
+#ifdef _WIN64
+  machineType = IMAGE_FILE_MACHINE_IA64;
+  stack.AddrPC.Offset = context.Rip;
+  stack.AddrPC.Mode = AddrModeFlat;
+  stack.AddrStack.Offset = context.Rsp;
+  stack.AddrStack.Mode = AddrModeFlat;
+  stack.AddrFrame.Offset = context.Rbp;
+  stack.AddrFrame.Mode = AddrModeFlat;
+#else
+  machineType = IMAGE_FILE_MACHINE_I386;
+  stack.AddrPC.Offset = context.Eip;
+  stack.AddrPC.Mode = AddrModeFlat;
+  stack.AddrStack.Offset = context.Esp;
+  stack.AddrStack.Mode = AddrModeFlat;
+  stack.AddrFrame.Offset = context.Ebp;
+  stack.AddrFrame.Mode = AddrModeFlat;
+#endif
+
+  do {
+    result = StackWalk64(machineType,
+                         process,
+                         thread,
+                         &stack,
+                         NULL,
+                         //&context,
+                         NULL,
+                         SymFunctionTableAccess64,
+                         SymGetModuleBase64,
+                         NULL);
+
+    symbol->SizeOfStruct = sizeof(IMAGEHLP_SYMBOL64);
+    symbol->MaxNameLength = symbol_name_size - 1;
+
+    if (stack.AddrPC.Offset != 0) {
+      SymGetSymFromAddr64(
+          process, (ULONG64)stack.AddrPC.Offset, &displacement, symbol);
+      // UnDecorateSymbolName(
+      //     symbol->Name, (PSTR)&name[0], name.size(), UNDNAME_COMPLETE);
+
+      IMAGEHLP_LINE64 line{};
+      line.SizeOfStruct = sizeof(IMAGEHLP_LINE64);
+      DWORD line_displacement = 0;
+      auto res = SymGetLineFromAddr64(
+          process, (ULONG64)stack.AddrPC.Offset, &line_displacement, &line);
+
+      out += symbol->Name;
+
+      if (res) {
+        out += ':';
+        out += std::to_string(line.LineNumber);
+        out += ':';
+        out += std::to_string(line_displacement);
+        out += " in ";
+        out += line.FileName;
+      }
+      out += '\n';
+    }
+
+  } while (result);
+
+  return out;
+}
+
+static LONG WINAPI SEHFilterFunc(LPEXCEPTION_POINTERS exceptions) {
+  VLOG(1) << __LINE__;
+  std::cerr << "Error Code: " << exceptions->ExceptionRecord->ExceptionCode
+            << std::endl;
+
+  std::cerr << getStack(*exceptions->ContextRecord);
+
+  return EXCEPTION_EXECUTE_HANDLER;
+}
+
 TEST_F(Secureboot, test_sanity) {
   bool secureboot_supported{false};
+
+  static const auto result = SymInitialize(GetCurrentProcess(), nullptr, TRUE);
+
+  ASSERT_TRUE(result);
+
+  SetUnhandledExceptionFilter(SEHFilterFunc);
 
   {
     auto platform_info_rows =
